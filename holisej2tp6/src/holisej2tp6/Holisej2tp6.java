8914ee0cfe4cab4497040984c9d3a3f2dcf9ba4a
doloresiravedra-tech <doloresiravedra@gmail.com>
2026-07-16 12:20:41 -0300

    lol

---
+++ holisej2tp6/src/holisej2tp6/Holisej2tp6.java
@@ -0,0 +1,11 @@
+package holisej2tp6;
+public class Holisej2tp6 {
+    public static void main(String[] args) {
+
+Residente residente = new Residente();
+Producto producto = new Producto();
+Paciente paciente = new Paciente();
+Pasajero pasajero = new Pasajero();
+
+}
+}
